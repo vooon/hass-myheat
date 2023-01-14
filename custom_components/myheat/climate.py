73d03a9ce0a4c0eec99a83b0411e3afab19c78ee
Vladimir Ermakov <vooon341@gmail.com>
2023-01-14 23:45:41 +0100

    climate: use attrs

--- custom_components/myheat/climate.py
+++ custom_components/myheat/climate.py
@@ -1,6 +1,7 @@
 """Climate platform for MyHeat."""
 
-from homeassistant.const import UnitOfTemperature
+import logging
+
 from homeassistant.components.climate import (
     PRESET_COMFORT,
     PRESET_ECO,
@@ -10,10 +11,10 @@ from homeassistant.components.climate import (
     HVACAction,
     HVACMode,
 )
+from homeassistant.const import UnitOfTemperature
 
-from .const import CONF_NAME, DEFAULT_NAME, DOMAIN, ICON, Platform
+from .const import CONF_NAME, DEFAULT_NAME, DOMAIN
 from .entity import MhEntity
-import logger
 
 _logger = logging.getLogger(__package__)
 
@@ -39,21 +40,62 @@ class MhEnvClimate(MhEntity, ClimateEntity):
         super().__init__(coordinator, config_entry)
         self.env = env
 
+        self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
+
+        self._attr_current_humidity = None
+        self._attr_current_temperature = None
+
+        self._attr_fan_mode = None
+        self._attr_fan_modes = []
+
+        self._attr_hvac_action = None
+        self._attr_hvac_mode = HVACMode.AUTO
+        self._attr_hvac_modes = [HVACMode.OFF, HVACMode.HEAT, HVACMode.AUTO]
+
+        self._attr_is_aux_heat = False
+
+        # self._attr_max_humidity = DEFAULT_MAX_HUMIDITY
+        # self._attr_max_temp: float
+        # self._attr_min_humidity: int = DEFAULT_MIN_HUMIDITY
+        # self._attr_min_temp: float
+
+        # self._attr_precision: float
+        # self._attr_preset_mode: str | None
+        # self._attr_preset_modes: list[str] | None
+
+        # self._attr_swing_mode: str | None
+        # self._attr_swing_modes: list[str] | None
+
+        # self._attr_target_humidity = None
+        # self._attr_target_temperature_high = None
+        # self._attr_target_temperature_low = None
+        self._attr_target_temperature_step = 1.0
+        self._attr_target_temperature = None
+
+        self._attr_temperature_unit = UnitOfTemperature.CELSIUS
+
     @property
     def name(self):
         """Return the name of the sensor."""
         name = self.config_entry.data.get(CONF_NAME, DEFAULT_NAME)
         return f"{name} {self.env['name']}"
 
-    @property
-    def temperature_unit(self) -> str:
-        """Return the unit of measurement."""
-        return UnitOfTemperature.CELSIUS
+    @callback
+    def _handle_coordinator_update(self):
+        """Get the latest state from the thermostat."""
 
-    @property
-    def current_temperature(self):
-        """Return the current temperature."""
-        return self._env().get("value")
+        e = self._env()
+
+        self._attr_current_temperature = e.get("value")
+        self._attr_target_temperature = e.get("target")
+
+        self._attr_hvac_action = (
+            HVACAction.HEATING
+            if self._attr_target_temperature is not None
+            else HVACMode.OFF
+        )
+
+        self.async_write_ha_state()
 
     # @property
     # def device_class(self):
