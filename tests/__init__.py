1bc2558f4912170767eeb152cabcb0542fd3f404
Vladimir Ermakov <vooon341@gmail.com>
2023-01-14 14:08:06 +0100

    Initial import
    > cookiecutter gh:oncleben31/cookiecutter-homeassistant-custom-component --checkout=2021.1.1

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for MyHeat integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for MyHeat tests."""
+from custom_components.myheat.const import (
+    CONF_PASSWORD,
+)
+from custom_components.myheat.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
