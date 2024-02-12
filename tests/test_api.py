369f29b4d34c55b225faf963ab79d8800d8c4f5d
Vladimir Ermakov <vooon341@gmail.com>
2024-02-12 12:48:28 +0100

    tests: separate api calls

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,9 +3,10 @@
 import asyncio
 
 import aiohttp
-from custom_components.myheat.api import MhApiClient
-from custom_components.myheat.api import RPC_ENDPOINT
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import pytest
+
+from custom_components.myheat.api import RPC_ENDPOINT, MhApiClient
 
 # Use data from api pdf
 GET_DEVIDES = {
@@ -169,39 +170,54 @@ GET_DEVIDE_INFO = {
 NO_ERR = {"err": 0, "refreshPage": False}
 
 
-async def test_api(hass, aioclient_mock, caplog):
-    """Test API calls."""
-
-    api = MhApiClient(
+@pytest.fixture
+def api_client(hass) -> MhApiClient:
+    return MhApiClient(
         username="test_user",
         api_key="test_api_key",
         device_id=12,
         session=async_get_clientsession(hass),
     )
 
+
+async def test_api_get_devices(api_client, aioclient_mock, caplog):
+    """Test API calls."""
+
     aioclient_mock.post(RPC_ENDPOINT, json=GET_DEVIDES)
-    assert await api.async_get_devices() == GET_DEVIDES["data"]
+    assert await api_client.async_get_devices() == GET_DEVIDES["data"]
+
+
+async def test_api_get_device_info(api_client, aioclient_mock, caplog):
+    """Test API calls."""
 
     aioclient_mock.post(RPC_ENDPOINT, json=GET_DEVIDE_INFO)
-    assert await api.async_get_device_info() == GET_DEVIDE_INFO["data"]
+    assert await api_client.async_get_device_info() == GET_DEVIDE_INFO["data"]
+
+
+async def test_api_set(api_client, aioclient_mock, caplog):
+    """Test API calls."""
 
     aioclient_mock.post(RPC_ENDPOINT, json=NO_ERR)
-    assert await api.async_set_env_goal(obj_id=21, goal=24) is None
+    assert await api_client.async_set_env_goal(obj_id=21, goal=24) is None
 
     aioclient_mock.post(RPC_ENDPOINT, json=NO_ERR)
-    assert await api.async_set_env_curve(obj_id=21, curve=0) is None
+    assert await api_client.async_set_env_curve(obj_id=21, curve=0) is None
 
     aioclient_mock.post(RPC_ENDPOINT, json=NO_ERR)
-    assert await api.async_set_eng_goal(obj_id=21, goal=-1) is None
+    assert await api_client.async_set_eng_goal(obj_id=21, goal=-1) is None
 
     aioclient_mock.post(RPC_ENDPOINT, json=NO_ERR)
-    assert await api.async_set_heating_mode(mode_id=2) is None
+    assert await api_client.async_set_heating_mode(mode_id=2) is None
 
     aioclient_mock.post(RPC_ENDPOINT, json=NO_ERR)
-    assert await api.async_set_heating_mode(mode_id=0, schedule_id=1) is None
+    assert await api_client.async_set_heating_mode(mode_id=0, schedule_id=1) is None
 
     aioclient_mock.post(RPC_ENDPOINT, json=NO_ERR)
-    assert await api.async_set_security_mode(mode=True) is None
+    assert await api_client.async_set_security_mode(mode=True) is None
+
+
+async def test_api_rpc_errors(api_client, aioclient_mock, caplog):
+    """Test API calls."""
 
     # In order to get 100% coverage, we need to test `api_wrapper` to test the code
     # that isn't already called by `async_get_data` and `async_set_title`. Because the
@@ -213,7 +229,7 @@ async def test_api(hass, aioclient_mock, caplog):
     # exception handling is a logging statement
     caplog.clear()
     aioclient_mock.post(RPC_ENDPOINT, exc=asyncio.TimeoutError)
-    assert await api.rpc("timeout") is None
+    assert await api_client.rpc("timeout") is None
     assert (
         len(caplog.record_tuples) == 1
         and "Timeout error fetching information from" in caplog.record_tuples[0][2]
@@ -221,7 +237,7 @@ async def test_api(hass, aioclient_mock, caplog):
 
     caplog.clear()
     aioclient_mock.post(RPC_ENDPOINT, exc=aiohttp.ClientError)
-    assert await api.rpc("client_error") is None
+    assert await api_client.rpc("client_error") is None
     assert (
         len(caplog.record_tuples) == 1
         and "Error fetching information from" in caplog.record_tuples[0][2]
@@ -229,7 +245,7 @@ async def test_api(hass, aioclient_mock, caplog):
 
     caplog.clear()
     aioclient_mock.post(RPC_ENDPOINT, exc=Exception)
-    assert await api.rpc("exc") is None
+    assert await api_client.rpc("exc") is None
     assert (
         len(caplog.record_tuples) == 1
         and "Something really wrong happened!" in caplog.record_tuples[0][2]
@@ -237,7 +253,7 @@ async def test_api(hass, aioclient_mock, caplog):
 
     caplog.clear()
     aioclient_mock.post(RPC_ENDPOINT, exc=TypeError)
-    assert await api.rpc("type_error") is None
+    assert await api_client.rpc("type_error") is None
     assert (
         len(caplog.record_tuples) == 1
         and "Error parsing information from" in caplog.record_tuples[0][2]
