67370006ce010a6e5a8574c0ffb6d30a799aef49
Vladimir Ermakov <vooon341@gmail.com>
2025-05-18 18:47:50 +0200

    Bump version: 0.5.1 → 0.6.0

--- custom_components/myheat/const.py
+++ custom_components/myheat/const.py
@@ -10,7 +10,7 @@ from homeassistant.const import Platform
 NAME = "MyHeat.net"
 DOMAIN = "myheat"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.5.1"
+VERSION = "0.6.0"
 
 ATTRIBUTION = "https://myheat.net"
 MANUFACTURER = "https://myheat.net"
