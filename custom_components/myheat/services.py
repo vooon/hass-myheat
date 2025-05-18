76bc4ac244f1318ef4588ef1833e8fe8991c6940
Vladimir Ermakov <vooon341@gmail.com>
2025-05-18 13:37:19 +0200

    register services

--- custom_components/myheat/services.py
+++ custom_components/myheat/services.py
@@ -12,5 +12,35 @@ async def async_get_devices(call: ServiceCall) -> ServiceResponse:
     return
 
 
+async def async_get_device_info(call: ServiceCall) -> ServiceResponse:
+    return
+
+
+async def async_set_env_goal(call: ServiceCall) -> ServiceResponse:
+    return
+
+
+async def async_set_env_curve(call: ServiceCall) -> ServiceResponse:
+    return
+
+
+async def async_set_eng_goal(call: ServiceCall) -> ServiceResponse:
+    return
+
+
+async def async_set_heating_mode(call: ServiceCall) -> ServiceResponse:
+    return
+
+
+async def async_set_security_mode(call: ServiceCall) -> ServiceResponse:
+    return
+
+
 async def async_setup_services(hass: HomeAssistant):
     hass.services.async_register(DOMAIN, "get_devices", async_get_devices)
+    hass.services.async_register(DOMAIN, "get_device_info", async_get_device_info)
+    hass.services.async_register(DOMAIN, "set_env_goal", async_set_env_goal)
+    hass.services.async_register(DOMAIN, "set_env_curve", async_set_env_curve)
+    hass.services.async_register(DOMAIN, "set_eng_goal", async_set_eng_goal)
+    hass.services.async_register(DOMAIN, "set_heating_mode", async_set_heating_mode)
+    hass.services.async_register(DOMAIN, "set_security_mode", async_set_security_mode)
